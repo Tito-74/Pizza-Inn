99f429cef8f5b0a4add82c410bb961a80f4a4828
Tito-74 <kipkirui133@gmail.com>
2021-08-01 15:14:14 +0300

    adding constractor

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function PizzaOrder(size,type,crust,topping,quantity){
+  this.size = size;
+  this.pizzaType = type;
+  this.crust =crust;
+  this.topping=topping;
+  this.quantity = quantity;
+  }
